ef33f9861db7f1a23686e4e7d50bdd22fc55f73b
kanmuteh1 <jameskgayejr@gmail.com>
2020-11-16 15:08:54 +0000

    updated tic-tac-toe

--- tic-tac-toe/javascript/game-on.js
+++ tic-tac-toe/javascript/game-on.js
@@ -1,59 +1,33 @@
-// selecting player character
-var playerX = document.querySelectorAll("#player-x");
-console.log(playerX);
-
-var playerO = document.querySelectorAll("#player-o");
-console.log(playerO);
+ // selecting player character
+function playerSelection(){
+   var playerX = document.querySelectorAll("#player-x");
+   var playerO = document.querySelectorAll("#player-o");
+}
 
 var getPlayingSpot = document.querySelectorAll(".playing-spot");
-Array.from(getPlayingSpot).forEach(function(getPlayingSpot){
+Array.from(getPlayingSpot).forEach(function(getPlayingSpot, index , array){
    getPlayingSpot.addEventListener("click", function(spot){
-        while(getPlayingSpot.innerHTML === ""){
-            if(getPlayingSpot.innerHTML === ""){
-                spot.target.innerHTML = "o";
-            }
-            var computer = Math.floor(Math.random()*8)+0;
-
-            if(computer === 0){
-                document.getElementById("0").innerHTML = "x";
-            }
-
-            else if(computer === 1){
-                document.getElementById("1").innerHTML = "x";
-            }
-
-            else if(computer === 2){
-                document.getElementById("2").innerHTML = "x";
-            }
-
-            else if(computer === 3){
-                document.getElementById("3").innerHTML = "x";
-            }
-
-            else if(computer === 4){
-                document.getElementById("4").innerHTML = "x";
-            }
-
-            else if(computer === 5){
-                document.getElementById("5").innerHTML = "x";
-            }
-
-            else if(computer === 6){
-                document.getElementById("6").innerHTML = "x";
-            }
-
-            else if(computer === 7){
-                document.getElementById("7").innerHTML = "x";
-            }
-
-            else if(computer === 8){
-                document.getElementById("8").innerHTML = "x";
-            }
-
-        }
-   })
-
-   
+      getPlayingSpot.innerHTML = "o"
+      // array.innerHTML = "o";
+      computer();
+   }) 
 })
-    
-        
+
+
+function generateRandomNum(){
+   var computerSelection = Math.floor(Math.random()*8)+0;
+   return computerSelection;
+}
+function computer(){
+   generateRandomNum()
+   if (getPlayingSpot[generateRandomNum()].innerHTML === ""){
+      getPlayingSpot[generateRandomNum()].innerHTML = "x";
+   }
+
+   // else if(getPlayingSpot[generateRandomNum()].innerHTML === "o"){
+   //    getPlayingSpot[generateRandomNum()].innerHTML === "o"
+   //   generateRandomNum()
+   //    getPlayingSpot[computerSelection].innerHTML === ""
+   //    getPlayingSpot[computerSelection].innerHTML === "x"
+   // }
+}
