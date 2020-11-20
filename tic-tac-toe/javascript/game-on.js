d37e9873eff15c63116d79bf0db412de274082a0
kanmuteh1 <jameskgayejr@gmail.com>
2020-11-20 11:17:42 +0000

    latest update Tic-tac-toe

--- tic-tac-toe/javascript/game-on.js
+++ tic-tac-toe/javascript/game-on.js
@@ -1,7 +1,8 @@
- // selecting player character
-function playerCatSelection(select){
-   var players = document.getElementById(select.target.id);
-}
+//  // selecting player character
+//  var players;
+//  function playerCatSelection(select){
+//    players = document.getElementById(select.target.id).innerHTML;
+// }
 
 var check_boxes = ["0","1","2","3","4","5","6","7","8"];
 
@@ -9,35 +10,114 @@ function humanPlay(human){
    var humanId = document.getElementById(human.target.id);
    if(humanId.innerHTML === ""){
       humanId.innerHTML = "o";
-      check_boxes.forEach(function(remove){
-         if(humanId.id === remove){
-            var a = check_boxes.splice(remove,1);
-            console.log(a);
-            // console.log("human",check_boxes)
-         }
-      })
+      var items_index1 = check_boxes.indexOf(humanId.id).toString();
+      check_boxes.splice(items_index1,1);
    }
-   generateRandomNum()
+   humanResults();
+   generateRandomNum();
 }
 
 function generateRandomNum(){
+   var items_index;
    var computer_selection = Math.floor(Math.random()*8);
    computer_selection = computer_selection.toString();
    var computer_player = document.getElementById(computer_selection);
    if(computer_player.innerHTML === ""){
       computer_player.innerHTML = "x";
+      items_index = check_boxes.indexOf(computer_player.id);
+      check_boxes.splice(items_index,1);
 
    }
-
    else{
+      if(check_boxes.length > 0){
       generateRandomNum();
-      check_boxes.forEach(function(remove){
-         if(computer_player.id === remove){
-            var b = check_boxes.splice(remove,1);
-            console.log(b)
-            // console.log("computer",check_boxes)
-         }
-      })
+      }
+   
       return
    }
+   humanResults();
+}
+
+function resetAll(){
+   var resetAll = document.getElementById("start-over");
+   location.reload();
+}
+
+function humanResults(){
+   var a = document.getElementById("0");
+   var b = document.getElementById("1");
+   var c = document.getElementById("2");
+   var d = document.getElementById("3");
+   var e = document.getElementById("4");
+   var f = document.getElementById("5");
+   var g = document.getElementById("6");
+   var h = document.getElementById("7");
+   var i = document.getElementById("8");
+   if(a.innerHTML === "x" && b.innerHTML === "x" && c.innerHTML === "x"){
+      alert("computer win")
+   }
+
+   else if(a.innerHTML === "o" && b.innerHTML === "o" && c.innerHTML === "o"){
+      alert("you win")
+   }
+
+   else if(d.innerHTML === "x" && e.innerHTML === "x" && f.innerHTML === "x"){
+      alert("computer win")
+   }
+
+   else if(d.innerHTML === "o" && e.innerHTML === "o" && f.innerHTML === "o"){
+      alert("you win")
+   }
+
+   else if(g.innerHTML === "x" && h.innerHTML === "x" && i.innerHTML === "x"){
+      alert("you win")
+   }
+
+   else if(g.innerHTML === "o" && h.innerHTML === "o" && i.innerHTML === "o"){
+      alert("you win")
+   }
+
+   else if(a.innerHTML === "x" && d.innerHTML === "x" && g.innerHTML === "x"){
+      alert("computer win")
+   }
+
+   else if(a.innerHTML === "o" && d.innerHTML === "o" && g.innerHTML === "o"){
+      alert("you win")
+   }
+
+   else if(b.innerHTML === "x" && e.innerHTML === "x" && h.innerHTML === "x"){
+      alert("computer win")
+   }
+
+   else if(b.innerHTML === "o" && e.innerHTML === "o" && h.innerHTML === "o"){
+      alert("you win")
+   }
+
+   else if(c.innerHTML === "x" && f.innerHTML === "x" && i.innerHTML === "x"){
+      alert("computer win")
+   }
+
+   else if(c.innerHTML === "o" && f.innerHTML === "o" && i.innerHTML === "o"){
+      alert("you win")
+   }
+
+   else if(c.innerHTML === "x" && e.innerHTML === "x" && g.innerHTML === "x"){
+      alert("computer win")
+   }
+
+   else if(c.innerHTML === "o" && e.innerHTML === "o" && g.innerHTML === "o"){
+      alert("you win")
+   }
+
+   else if(a.innerHTML === "x" && e.innerHTML === "x" && i.innerHTML === "x"){
+      alert("computer win")
+   }
+
+   else if(a.innerHTML === "o" && e.innerHTML === "o" && i.innerHTML === "o"){
+      alert("you win")
+   }
+
+   // else{
+   //    alert("tie")
+   // }
 }
