92f59aae6e624aab36385e440604047e0a82d93b
kanmuteh1 <jameskgayejr@gmail.com>
2020-11-22 19:46:02 +0000

    this is my latest update on the tic-tac-toe

--- tic-tac-toe/javascript/game-on.js
+++ tic-tac-toe/javascript/game-on.js
@@ -1,15 +1,30 @@
-//  // selecting player character
-//  var players;
-//  function playerCatSelection(select){
-//    players = document.getElementById(select.target.id).innerHTML;
-// }
+ // selecting player character
+ var human_name;
+ var computer_name;
+ function playerCatSelection(select){
+   human_name = document.getElementById(select.target.id).innerHTML;
+   if(human_name === "x"){
+      computer_name = "o";
+   }
+
+   else if(human_name === "o"){
+      computer_name = "x";
+   }
+  
+}
+
+
+var players_scores = document.querySelectorAll(".players")
+
+var score;
 
 var check_boxes = ["0","1","2","3","4","5","6","7","8"];
 
 function humanPlay(human){
    var humanId = document.getElementById(human.target.id);
    if(humanId.innerHTML === ""){
-      humanId.innerHTML = "o";
+      human_name = humanId.innerHTML;
+      console.log(human_name)
       var items_index1 = check_boxes.indexOf(humanId.id).toString();
       check_boxes.splice(items_index1,1);
    }
@@ -21,9 +36,10 @@ function generateRandomNum(){
    var items_index;
    var computer_selection = Math.floor(Math.random()*8);
    computer_selection = computer_selection.toString();
-   var computer_player = document.getElementById(computer_selection);
+   var computer_player = document.getElementById(computer_selection);          
    if(computer_player.innerHTML === ""){
-      computer_player.innerHTML = "x";
+      computer_name = computer_player.innerHTML;
+      console.log(computer_name)
       items_index = check_boxes.indexOf(computer_player.id);
       check_boxes.splice(items_index,1);
 
@@ -39,8 +55,11 @@ function generateRandomNum(){
 }
 
 function resetAll(){
-   var resetAll = document.getElementById("start-over");
-   location.reload();
+   var reset = document.querySelectorAll(".playing-spot");
+   for (var remove = 0; remove <= reset.length; remove++){
+      reset[remove].innerHTML = "";
+      check_boxes = ["0","1","2","3","4","5","6","7","8"]
+   }
 }
 
 function humanResults(){
@@ -55,10 +74,12 @@ function humanResults(){
    var i = document.getElementById("8");
    if(a.innerHTML === "x" && b.innerHTML === "x" && c.innerHTML === "x"){
       alert("computer win")
+      score++;
    }
 
    else if(a.innerHTML === "o" && b.innerHTML === "o" && c.innerHTML === "o"){
       alert("you win")
+      score++;
    }
 
    else if(d.innerHTML === "x" && e.innerHTML === "x" && f.innerHTML === "x"){
@@ -67,38 +88,47 @@ function humanResults(){
 
    else if(d.innerHTML === "o" && e.innerHTML === "o" && f.innerHTML === "o"){
       alert("you win")
+      score++;
    }
 
    else if(g.innerHTML === "x" && h.innerHTML === "x" && i.innerHTML === "x"){
       alert("you win")
+      score++;
    }
 
    else if(g.innerHTML === "o" && h.innerHTML === "o" && i.innerHTML === "o"){
       alert("you win")
+      score++;
    }
 
    else if(a.innerHTML === "x" && d.innerHTML === "x" && g.innerHTML === "x"){
       alert("computer win")
+      score++;
    }
 
    else if(a.innerHTML === "o" && d.innerHTML === "o" && g.innerHTML === "o"){
       alert("you win")
+      score++;
    }
 
    else if(b.innerHTML === "x" && e.innerHTML === "x" && h.innerHTML === "x"){
       alert("computer win")
+      score++;
    }
 
    else if(b.innerHTML === "o" && e.innerHTML === "o" && h.innerHTML === "o"){
       alert("you win")
+      score++;
    }
 
    else if(c.innerHTML === "x" && f.innerHTML === "x" && i.innerHTML === "x"){
       alert("computer win")
+      score++;
    }
 
    else if(c.innerHTML === "o" && f.innerHTML === "o" && i.innerHTML === "o"){
       alert("you win")
+      score++;
    }
 
    else if(c.innerHTML === "x" && e.innerHTML === "x" && g.innerHTML === "x"){
@@ -107,17 +137,16 @@ function humanResults(){
 
    else if(c.innerHTML === "o" && e.innerHTML === "o" && g.innerHTML === "o"){
       alert("you win")
+      score++;
    }
 
    else if(a.innerHTML === "x" && e.innerHTML === "x" && i.innerHTML === "x"){
       alert("computer win")
+      score++;
    }
 
    else if(a.innerHTML === "o" && e.innerHTML === "o" && i.innerHTML === "o"){
       alert("you win")
+      score++;
    }
-
-   // else{
-   //    alert("tie")
-   // }
 }
