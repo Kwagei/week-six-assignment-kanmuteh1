90118ca857a64083a2b6e2cc2e9904c5d045c1c0
kanmuteh1 <jameskgayejr@gmail.com>
2020-11-26 11:07:58 +0000

    latest update Tic-tac-toe project

--- tic-tac-toe/javascript/tic-tac-toe.js
+++ tic-tac-toe/javascript/tic-tac-toe.js
@@ -158,7 +158,7 @@ function resetAll(){
       check_boxes = ["0","1","2","3","4","5","6","7","8"];
       result.style.display = "none";
       first_page.style.display = "block";
-
+      game_on.style.display = "none";
    }
    
 }
